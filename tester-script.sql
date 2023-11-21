50fc0f174f4924d4634b3448184740938945902b
robinmth <robinmth@student.ubc.ca>
2023-11-21 14:40:26 -0800

    small updates

--- tester-script.sql
+++ tester-script.sql
@@ -1,4 +1,5 @@
-ALTER TABLE AnimalCaretaker DROP CONSTRAINT ac_fk;
+
+ALTER TABLE AnimalCaretaker DROP CONSTRAINT ac_fk_fe;
 DROP TABLE FundraiserEvent;
 
 ALTER TABLE Animal DROP CONSTRAINT a_fk_fc;
@@ -23,22 +24,20 @@ DROP TABLE Worker;
 DROP TABLE Volunteer;
 
 
-CREATE TABLE FundraiserEvent (eventID NUMBER CONSTRAINT fe_pk PRIMARY KEY, eventType VARCHAR(50), eventDayTime TIMESTAMP CONSTRAINT event_u UNIQUE, donationGoal NUMBER);
+CREATE TABLE FundraiserEvent (eventID INTEGER CONSTRAINT fe_pk PRIMARY KEY, eventType VARCHAR2(50), eventDayTime DATE CONSTRAINT fe_u UNIQUE, donationGoal NUMBER);
 
 CREATE TABLE AnimalCaretaker (caretakerID INTEGER CONSTRAINT ac_pk PRIMARY KEY, caretakerName VARCHAR2(100), fundEventID INTEGER CONSTRAINT ac_fk_fe REFERENCES FundraiserEvent(eventID), caretakerAddress VARCHAR2(50), caretakerPostalCode VARCHAR2(8));
 
 CREATE TABLE Customer (customerID INTEGER CONSTRAINT c_pk PRIMARY KEY, customerName VARCHAR2(100));
 
-CREATE TABLE Adopter (adopterID INTEGER CONSTRAINT a_pk PRIMARY KEY REFERENCES Customer(customerID), numOfAdoptions INTEGER, safeOwnerRating INTEGER, adopterPostalCode VARCHAR2(8), adopterAddress VARCHAR2(50));
+CREATE TABLE Adopter (adopterID INTEGER CONSTRAINT a_pk PRIMARY KEY REFERENCES Customer(customerID) ON DELETE CASCADE, numOfAdoptions INTEGER, safeOwnerRating INTEGER, adopterPostalCode VARCHAR2(8), adopterAddress VARCHAR2(50));
 
 CREATE TABLE Animal (petID INTEGER CONSTRAINT animal_pk PRIMARY KEY, animalName VARCHAR2(50), type VARCHAR2(25), age INTEGER, favouriteCaretaker INTEGER CONSTRAINT a_fk_fc REFERENCES AnimalCaretaker(caretakerID), previousOwner INTEGER CONSTRAINT a_fk_po REFERENCES Customer(customerID), timeInShelter INTEGER, adopterID INTEGER CONSTRAINT a_fk_aid REFERENCES Adopter(adopterID));
 
-CREATE TABLE VetAppointment (vetDayTime TIMESTAMP CONSTRAINT va_pk PRIMARY KEY, vetLicenseID INTEGER, reason VARCHAR2(250), petID INTEGER CONSTRAINT va_fk REFERENCES Animal(petID));
+CREATE TABLE VetAppointment (vetDayTime DATE  CONSTRAINT va_pk PRIMARY KEY, vetLicenseID INTEGER, reason VARCHAR2(250), petID INTEGER CONSTRAINT va_fk REFERENCES Animal(petID));
 
 CREATE TABLE Worker (workerID INTEGER CONSTRAINT w_pk PRIMARY KEY REFERENCES AnimalCaretaker(caretakerID) ON DELETE CASCADE, hourlyPay INTEGER);
 
 CREATE TABLE Volunteer (volunteerID INTEGER CONSTRAINT v_pk PRIMARY KEY REFERENCES AnimalCaretaker(caretakerID) ON DELETE CASCADE, hoursVolunteered INTEGER);
 
 
-INSERT INTO FundraiserEvent (eventID, eventType, eventDayTime, donationGoal) VALUES (1, 'Charity Auction', '2023-11-01 18:00:00', 5000);
-INSERT INTO AnimalCaretaker (caretakerID, caretakerName, fundEventID, caretakerAddress, caretakerPostalCode) VALUES (1, 'John Peters', 1, '123 Main St', '12345');
