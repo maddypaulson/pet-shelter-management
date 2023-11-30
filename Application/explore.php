5faac2a94e7c80154fbeff6b434281ddf63a93f5
ubovict <ubovict@students.cs.ubc.ca>
2023-11-30 14:47:01 -0800

    edited nested query spec

--- Application/explore.php
+++ Application/explore.php
@@ -21,10 +21,10 @@
             <input type="submit" name="havingSubmit">
         </form>
 
-        <h2 id="nested">Nested Aggregation with GROUP BY: Average Number of Items purchased per Customer with quantity equal or above</h2>
+        <h2 id="nested">Nested Aggregation with GROUP BY: Average Number of Items purchased per Customer with quantity equal or above the average number of purchases of all Customers</h2>
         <form method="GET" action="explore.php"> <!-- refresh page when submitted -->
             <input type="hidden" id="nestedQueryRequest" name="nestedQueryRequest">
-            Minimum Quantity: <input type="text" name="nestedAvgQuantity"> <br /><br />
+            <!--Minimum Quantity: <input type="text" name="nestedAvgQuantity"> <br /><br />-->
             <input type="submit" name="nestedSubmit">
         </form>
 
